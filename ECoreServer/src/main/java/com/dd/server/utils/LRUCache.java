ff2136a2c4053c5a8e40b980e60c0802ecd209b0
wangshupeng <wangshupeng.dd@bytedance.com>
2019-01-23 13:54:20 +0800

    add lru cache

---
+++ ECoreServer/src/main/java/com/dd/server/utils/LRUCache.java
@@ -0,0 +1,81 @@
+package com.dd.server.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class LRUCache<K, V> {
+    private static final float hashTableLoadFactor = 0.75f;
+    private LinkedHashMap<K, V> map;
+    private int cacheSize;
+
+    /**
+     * Creates a new LRU cache. 在该方法中，new LinkedHashMap<K,V>(hashTableCapacity,
+     * hashTableLoadFactor, true)中，true代表使用访问顺序
+     *
+     * @param cacheSize the maximum number of entries that will be kept in this cache.
+     */
+    public LRUCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        int hashTableCapacity = (int) Math.ceil(cacheSize / hashTableLoadFactor) + 1;
+        map = new LinkedHashMap<K, V>(hashTableCapacity, hashTableLoadFactor, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+                return size() > LRUCache.this.cacheSize;
+            }
+        };
+    }
+
+    /**
+     * Retrieves an entry from the cache.<br>
+     * The retrieved entry becomes the MRU (most recently used) entry.
+     *
+     * @param key the key whose associated value is to be returned.
+     * @return the value associated to this key, or null if no value with this
+     * key exists in the cache.
+     */
+    public synchronized V get(K key) {
+        return map.get(key);
+    }
+
+    /**
+     * Adds an entry to this cache. The new entry becomes the MRU (most recently
+     * used) entry. If an entry with the specified key already exists in the
+     * cache, it is replaced by the new entry. If the cache is full, the LRU
+     * (least recently used) entry is removed from the cache.
+     *
+     * @param key   the key with which the specified value is to be associated.
+     * @param value a value to be associated with the specified key.
+     */
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    /**
+     * Clears the cache.
+     */
+    public synchronized void clear() {
+        map.clear();
+    }
+
+    /**
+     * Returns the number of used entries in the cache.
+     *
+     * @return the number of entries currently in the cache.
+     */
+    public synchronized int size() {
+        return map.size();
+    }
+
+    /**
+     * Returns a <code>Collection</code> that contains a copy of all cache
+     * entries.
+     *
+     * @return a <code>Collection</code> with a copy of the cache content.
+     */
+    public synchronized Collection<Map.Entry<K, V>> getAll() {
+        return new ArrayList<>(map.entrySet());
+    }
+}
